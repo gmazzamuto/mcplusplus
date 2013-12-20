cd7c7706c8b565701823d0bb285980eaa8ff10ec
Lorenzo Pattelli <pattelli@lens.unifi.it>
2013-12-20 22:50:26 +0100

    random number generation test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <boost/random/variate_generator.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/discrete_distribution.hpp>
+#include <boost/random/uniform_real.hpp>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World\n");
+    int seed = 0;
+
+    double sigma = 0.5;
+
+    boost::variate_generator<boost::mt19937, boost::normal_distribution<> >
+            generator(boost::mt19937(seed),
+                      boost::normal_distribution<>(0,sigma));
+
+
+    for (int i = 0; i < 1000; ++i) {
+        printf("%f\n",generator());
+    }
+
     return 0;
 }
