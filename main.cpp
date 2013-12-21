7e3df121bbba8090fd9ec50320fc1a8ce2541478
Lorenzo Pattelli <pattelli@lens.unifi.it>
2013-12-21 02:09:27 +0100

    prints two columns of CosTheta values according to both distributions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "uniformrandomgenerator.h"
+#include "costhetagenerator.h"
 
 int main(int argc, char *argv[])
 {
-    UniformRandomGenerator *uGenerator = new UniformRandomGenerator(0);
+
+    IsotropicCosThetaGenerator *isct = new IsotropicCosThetaGenerator(0);
+    AnisotropicCosThetaGenerator *anct = new AnisotropicCosThetaGenerator(0,0.99);
 
     for (int i = 0; i < 100; ++i) {
-        printf("%f\n", uGenerator->spin());
+        printf("%lf\t%lf\n", isct->spin(),anct->spin());
     }
 
     return 0;
