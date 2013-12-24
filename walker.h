605affe3999fbfe0039d8678731e2c75ff1a831e
Lorenzo Pattelli <pattelli@lens.unifi.it>
2013-12-24 19:34:36 +0100

    walker.cpp: add variable descriptions

--- walker.h
+++ walker.h
@@ -5,12 +5,12 @@ class Walker
 {
 public:
     Walker();
-    double r0[3];
-    double r1[3];
-    double k0[3];
-    double k1[3];
-    double walkTime;
-    double walkDistance;
+    double r0[3];  /**< @brief position before the \f$ n \f$-th step*/
+    double r1[3];  /**< @brief position after the \f$ n \f$-th step*/
+    double k0[3];  /**< @brief direction unit vector for the \f$ (n-1) \f$-th step*/
+    double k1[3];  /**< @brief direction unit vector for the \f$ n \f$-th step*/
+    double walkTime;  /**< @brief time elapsed up to the current position*/
+    double walkDistance;  /**< @brief distance covered to the current position*/
 };
 
 #endif // WALKER_H
