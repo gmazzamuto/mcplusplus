b65e8528512023ba2d666d334dfeb65857f3f55d
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-01-21 00:25:23 +0100

    Simulation: first simple implementation of run()

--- simulation.cpp
+++ simulation.cpp
@@ -1,4 +1,12 @@
 #include "simulation.h"
+#include "distributions.h"
+#include "costhetagenerator.h"
+#include "psigenerator.h"
+
+#include <boost/math/special_functions/sign.hpp>
+
+using namespace boost;
+using namespace boost::math;
 
 Simulation::Simulation()
 {
@@ -25,4 +33,55 @@ void Simulation::setSample(Sample *sample) {
 
 void Simulation::setSource(Source *source) {
     this->source = source;
+}
+
+void Simulation::run() {
+    mt19937 *mt = new mt19937(0);
+    double totalThickness = sample->totalThickness();
+
+    int n = 0;
+
+    ExponentialDistribution *stepLength = new ExponentialDistribution(mt);
+    IsotropicCosThetaGenerator *isotrCosTheta = new IsotropicCosThetaGenerator(mt);
+    PsiGenerator *randomPsi = new PsiGenerator(mt);
+
+    while(n < totalWalkers) {
+        Walker *walker = source->constructWalker();
+
+        while(1) {
+            double step = stepLength->spin();
+            double cosTheta = isotrCosTheta->spin();
+            double sinTheta = sqrt(1-pow(cosTheta,2));
+            double psi = randomPsi->spin();
+            double cosPsi = cos(psi);
+            double sinPsi = sin(psi);
+
+            if(fabs(walker->k0[2]) > 0.999999) {
+                walker->k1[0] = sinTheta*cosPsi;
+                walker->k1[1] = sinTheta*sinPsi;
+                walker->k1[2] = cosTheta*sign<double>(walker->k0[2]);
+            }
+            else { //FIXME
+                walker->k1[0] = sinTheta*cosPsi;
+                walker->k1[1] = sinTheta*sinPsi;
+                walker->k1[2] = cosTheta*sign<double>(walker->k0[2]);
+            }
+
+            walker->r1[0] = walker->r0[0] + step*walker->k1[0];
+            walker->r1[1] = walker->r0[1] + step*walker->k1[1];
+            walker->r1[2] = walker->r0[2] + step*walker->k1[2];
+
+            if(walker->r1[2] > totalThickness) {
+                transmitted++;
+                break;
+            }
+
+            if(walker->r1[2] < 0) {
+                reflected++;
+                break;
+            }
+        }
+
+        n++;
+    }
 }
