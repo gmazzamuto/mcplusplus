5df80dd5f74b40a13da4cf560650825714adac84
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-01-20 21:59:45 +0100

    document Sample::addLayer

--- sample.cpp
+++ sample.cpp
@@ -6,6 +6,16 @@ Sample::Sample()
     totalThickness = 0;
 }
 
+/**
+ * @brief Adds a layer to the sample.
+ * @param material
+ * @param thickness
+ *
+ *
+ * A layer is an infinitely extended slab with the given material and the given
+ * finite thickness (measured along \f$ z \f$).
+ */
+
 void Sample::addLayer(const Material &material, double thickness) {
     layers.push_back(material);
     zBoundaries.push_back(totalThickness + thickness);
