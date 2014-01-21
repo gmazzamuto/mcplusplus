c1425995b93da2ee7137e2be492e76ea19dbb44f
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-01-21 01:29:57 +0100

    main(): working simulation with 1 walker

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-    Simulation *sim = new Simulation();
-    Source *source = new PencilBeamSource();
+    Source *source = new Source();
+    DeltaDistribution *deltaZ = new DeltaDistribution(50);
+    DeltaDistribution *delta0 = new DeltaDistribution(0);
+    DeltaDistribution *delta1 = new DeltaDistribution(1);
+
+    AbstractDistribution *kDistr[3];
+    kDistr[0] = delta0;
+    kDistr[1] = delta0;
+    kDistr[2] = delta1;
+    source->setk0Distribution(kDistr);
+
+    AbstractDistribution *rDistr[3];
+    rDistr[0] = deltaZ;
+    rDistr[1] = deltaZ;
+    rDistr[2] = deltaZ;
+    source->setr0Distribution(rDistr);
+
+    source->setWalkTimeDistribution(delta0);
+
     Sample *sample = new Sample();
 
     Material *freeSpace = new FreeSpace();
@@ -16,8 +33,11 @@ int main(int argc, char *argv[])
     sample->addLayer(material,100);
     sample->setSurroundingEnvironment(freeSpace);
 
+    Simulation *sim = new Simulation();
+
     sim->setSample(sample);
     sim->setSource(source);
+    sim->setTotalWalkers(1);
 
     sim->run();
 
