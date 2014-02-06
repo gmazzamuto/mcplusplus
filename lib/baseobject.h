37f0a9c0c1b1b655a00153ee0ba8570792e90696
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-02-06 23:59:37 +0100

    BaseObject: disable copy constructor and assignment operator, update documentation accordingly

--- lib/baseobject.h
+++ lib/baseobject.h
@@ -3,22 +3,33 @@
 
 #include <cstddef>
 #include <list>
+#include <boost/utility.hpp>
 
 using namespace std;
 
 /**
  * @brief The BaseObject class is the base class for many MCPlusPlus objects.
  *
- * BaseObjects can have a parent, specified in the constructor or at a
- * later time with setParent(). In this case, the parent takes ownership of the
+ * BaseObjects can have a parent, specified in the constructor or at a later
+ * time with setParent(). In this case, the parent takes ownership of the
  * object and adds it to its own list of child objects.
  *
  * A parent / child relationship implies the following:
  * - when a child object is deleted, it is removed from the parent's child list
+ *
  * - when the parent object is deleted, all child objects are deleted too
+ *
+ * BaseObjects have neither a copy constructor nor an assignment operator (=).
+ * This means that you cannot copy a BaseObject into another BaseObject by a
+ * simple assignment and that you cannot use container classes (such as
+ * vectors) to store BaseObjects. In this case you will be forced to only use
+ * pointers to BaseObjects. For a nice explanation on this design choice, see
+ * Qt's <a
+ * href="http://qt-project.org/doc/qt-5.0/qtcore/object.html#identity-vs-value">
+ * Identity vs Value</a> which explains the same concept applied to QObjects.
  */
 
-class BaseObject
+class BaseObject : private boost::noncopyable
 {
 public:
     BaseObject(BaseObject *parent=NULL);
