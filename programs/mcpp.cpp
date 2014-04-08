51fc82d9d8f89a476364477476598d398f117d8c
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-04-08 11:56:39 +0200

    new executable mcpp, load simulation from xml file

---
+++ programs/mcpp.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "xmlparser.h"
+#include <QApplication>
+#include <QMainWindow>
+#include "simulationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    XMLParser parser;
+    Simulation *sim = parser.load(argv[1]);
+
+    sim->setTotalWalkers(atoi(argv[2]));
+    sim->setNThreads(atoi(argv[3]));
+
+    sim->setSeed(0);
+//    sim->setSaveTrajectoryEnabled(true);
+    sim->run();
+
+//    QApplication a(argc, argv);
+//    QMainWindow w;
+//    SimulationViewer simViewer(sim);
+
+//    simViewer.setDisplayedOriginPos(0,0,-600);
+//    simViewer.setDisplayedAxisLength(500);
+
+//    w.setCentralWidget(&simViewer);
+//    w.show();
+
+//    a.exec();
+
+    return 0;
+}
+
