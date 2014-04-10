5c95b118a7d4241b182da50da4175c6252657c92
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-04-10 11:33:25 +0200

    new class H5OutputFile

---
+++ lib/h5outputfile.cpp
@@ -0,0 +1,49 @@
+#include "h5outputfile.h"
+
+H5OutputFile::H5OutputFile()
+    : H5FileHelper()
+{
+}
+
+bool H5OutputFile::newFile(const char *fileName)
+{
+    bool ret = false;
+    ret = H5FileHelper::newFile(fileName);
+    if(!ret)
+        return false;
+
+    ret = createExitPointsDatasets();
+    if(!ret)
+        return false;
+    return true;
+}
+
+void H5OutputFile::appendTransmittedExitPoints(const MCfloat *buffer, const hsize_t size)
+{
+    openDataSet("exit-points/transmitted");
+
+    hsize_t dims[1];
+    *dims = *extentDims();
+    dims[0]--;
+
+    writeHyperSlab(dims,&size,buffer);
+}
+
+bool H5OutputFile::createExitPointsDatasets()
+{
+
+    newGroup("exit-points");
+
+    int ndims = 1;
+    hsize_t dims[ndims];
+
+    dims[0] = 1;
+    bool ret = false;
+    ret = newDataset("exit-points/reflected",ndims,dims,dims);
+    if(!ret)
+        return false;
+    ret = newDataset("exit-points/transmitted",ndims,dims,dims);
+    if(!ret)
+        return false;
+    return true;
+}
