45db460c97d48a35097814a2bda8dc5f90e92d80
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2014-07-03 10:08:11 +0200

    mcpp: add command line switches

--- programs/mcpp.cpp
+++ programs/mcpp.cpp
@@ -6,8 +6,62 @@
 #include "simulationviewer.h"
 #include "xmlparser.h"
 
+char *progName;
+
+void usage(FILE *f) {
+    fprintf(f, "\n"
+            "usage: %s [options] input_file"
+            "\n"
+            "\n"
+            "where input_file is an XML file or a H5 file\n"
+            "\n"
+            "[options]:\n"
+            "\t -h display help\n"
+            "\t -w number of walkers (long long int, defaults to 1000000000)\n"
+            "\t -t number of threads (defaults to 2)\n"
+            "\n", progName);
+}
+
 int main(int argc, char *argv[])
 {
+    uint nThreads = 1;
+    u_int64_t nWalkers = 1000000000;
+    progName = argv[0];
+
+    extern char *optarg;
+    extern int optind;
+
+    char *inputFileName = NULL;
+
+    int c;
+
+    while ((c = getopt(argc, argv, "hw:t:")) != -1)
+        switch (c) {
+        case 'h':
+            usage(stdout);
+            exit(EXIT_SUCCESS);
+            break;
+
+        case 'w':
+            nWalkers=atoll(optarg);
+            break;
+
+        case 't':
+            nThreads=atoi(optarg);
+            break;
+
+        default:
+            break;
+        }
+
+    if(argc - optind < 1) {
+        fprintf(stderr,"Error: input file not specified\n");
+        usage(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    inputFileName = argv[optind++];
+
     Simulation *sim;
 
     H5File h5File;
@@ -17,28 +71,28 @@ int main(int argc, char *argv[])
     string outputFileName;
     std::string xmlCmdLine;
 
-    if(access(argv[1],R_OK)<0) {
-        cerr << "ERROR: cannot access file " << argv[1] << endl;
+    if(access(inputFileName,R_OK)<0) {
+        cerr << "ERROR: cannot access file " << inputFileName << endl;
         exit(EXIT_FAILURE);
     }
 
     bool isValidH5 = false;
 
     try {
-        isValidH5 = h5File.isHdf5(argv[1]);
+        isValidH5 = h5File.isHdf5(inputFileName);
     }
     catch (Exception e) {}
 
     if(!isValidH5) //input file is XML
     {
         //read whole xml file to string
-        std::ifstream f(argv[1]);
+        std::ifstream f(inputFileName);
         xmlCmdLine = std::string((std::istreambuf_iterator<char>(f)),
                                  std::istreambuf_iterator<char>());
 
         parser = new XMLParser();
         try {
-            parser->parseFile(argv[1]);
+            parser->parseFile(inputFileName);
         } catch (XMLParser::Exception e) {
             cerr << "Error: " << e.str << endl;
             exit(EXIT_FAILURE);
@@ -50,7 +104,7 @@ int main(int argc, char *argv[])
         }
         if(access(outputFileName.c_str(),R_OK)<0) {//the h5 file does not exist
             cout << "Creating new file " << outputFileName << endl;
-            file.newFromXML(argv[1],outputFileName.c_str());
+            file.newFromXML(inputFileName,outputFileName.c_str());
             sim = parser->simulation();
             sim->setSeed(0);
             file.close();
@@ -59,7 +113,7 @@ int main(int argc, char *argv[])
             cout << "Appending to " << outputFileName << endl;
     }
     else { //input file is H5
-        outputFileName = argv[1];
+        outputFileName = inputFileName;
         cout << "Appending to " << outputFileName << endl;
     }
 
@@ -82,8 +136,8 @@ int main(int argc, char *argv[])
 
     file.close();
 
-    sim->setNWalkers(atoll(argv[2]));
-    sim->setNThreads(atoi(argv[3]));
+    sim->setNWalkers(nWalkers);
+    sim->setNThreads(nThreads);
 
     sim->setOutputFileName(outputFileName.c_str());
     sim->run();
