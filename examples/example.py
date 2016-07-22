4bc4c3dd8e6c644640a539b04370e7e5e1100413
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2016-07-22 11:27:32 +0200

    example: improve code style

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,7 @@
 
 from pymcplusplus import *
 
-#define a sample consisting of two layers of different materials
+# define a sample consisting of two layers of different materials
 sample = Sample()
 
 mat = Material()
@@ -10,26 +10,26 @@ mat.n = 1.5
 mat.g = 0
 mat.ls = 1000
 
-#add a layer of material mat and thickness 1000um
-sample.addLayer(mat,1000)
+# add a layer of material mat and thickness 1000um
+sample.addLayer(mat, 1000)
 
 mat2 = Material()
 mat2.n = 1.3
 mat.g = 0.5
 mat.ls = 700
 
-sample.addLayer(mat2,500)
+sample.addLayer(mat2, 500)
 
 externalMaterial = Material()
 externalMaterial.n = 1
 
 sample.setSurroundingEnvironment(externalMaterial)
 
-#define a photon source
+# define a photon source
 source = PencilBeamSource()
 
-#define the main simulation object, simulate 1e6 photons using 8 parallel threads.
-#note that each thread will use increasing seeds starting from 0
+# define the main simulation object, simulate 1e6 photons using 8 parallel
+# threads. Note that each thread will use increasing seeds starting from 0.
 sim = Simulation()
 sim.setSample(sample)
 sim.setSource(source)
@@ -38,7 +38,7 @@ sim.setNThreads(8)
 sim.setSeed(0)
 sim.setOutputFileName("example.h5")
 
-#define and add several histograms to the simulation
+# define and add several histograms to the simulation
 
 # 1) a histogram of the exit times
 hist = Histogram()
@@ -47,12 +47,12 @@ hist.setPhotonTypeFlags(FLAG_TRANSMITTED)
 hist.setMax(1000)
 hist.setBinSize(2)
 hist.setName("times")
-hist.addMomentExponent(2)  #compute spatial variance
+hist.addMomentExponent(2)  # compute spatial variance
 sim.addHistogram(hist)
 
 # 2) a histogram of the exit distances from the center
 hist = Histogram()
-hist.setDataDomain(DATA_POINTS);
+hist.setDataDomain(DATA_POINTS)
 hist.setPhotonTypeFlags(FLAG_TRANSMITTED)
 hist.setMax(1000000)
 hist.setBinSize(50)
@@ -63,8 +63,8 @@ sim.addHistogram(hist)
 hist = Histogram()
 hist.setDataDomain(DATA_POINTS, DATA_TIMES)
 hist.setPhotonTypeFlags(FLAG_TRANSMITTED)
-hist.setMax(100000,1000)
-hist.setBinSize(500,2)
+hist.setMax(100000, 1000)
+hist.setBinSize(500, 2)
 hist.setName("points_vs_times")
 sim.addHistogram(hist)
 
